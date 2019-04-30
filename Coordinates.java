8f71623763a69f6bcab64ac96a3e469539070ffb
Hikmet <hikmet.pirmamedov@mail.ru>
2019-04-30 18:21:33 +0400

    add class Coordinates

---
+++ Coordinates.java
@@ -0,0 +1,24 @@
+package NavalBattle;
+
+class Coordinates {
+    final int number;
+    final int letter;
+
+    public Coordinates(int number, int letter) {
+        this.number = number;
+        this.letter = letter;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 + ( number / letter);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Coordinates))
+            return false;
+        Coordinates c = (Coordinates) obj;
+        return (this.number == c.number) && (this.letter == c.letter);
+    }
+}
